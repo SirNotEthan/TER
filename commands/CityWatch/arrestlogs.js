1bf98c15627dd7496df17b7187ede6b21db149fc
SirNotEthan <ethn.bannister15@gmail.com>
2024-02-18 16:09:56 +0000

    Commit Message

---
+++ commands/CityWatch/arrestlogs.js
@@ -0,0 +1,69 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('arrest')
+    .setDescription('Log the user you have arrested.')
+    .addStringOption((option) => option 
+        .setName('username')
+        .setDescription('The roblox username of whom you arrested.')
+        .setRequired(true)
+    )
+    .addStringOption((option) => option
+        .setName('duration')
+        .setDescription('Duration of the arrest')
+        .setRequired(true)
+    )
+    .addStringOption((option) => option 
+        .setName('reason')
+        .setDescription('Reason of the arrest')
+        .setRequired(true)
+    )
+    .addStringOption((option) => option
+        .setName('evidence')
+        .setDescription('Evidence to be included of the arrest')
+        .setRequired(false)
+    ),
+
+    async execute(interaction) {
+        await interaction.deferReply()
+
+        if (!interaction.member.roles.cache.some(role => role.name === 'City Watch')) {
+            return interaction.editReply('You do not have permission to use this command.');
+        }
+
+        const username = interaction.options.getString('username');
+        const reason = interaction.options.getString('reason');
+        const duration = interaction.options.getString('duration');
+        const evidence = interaction.options.getString('evidence');
+
+        const LogsEmbed = new EmbedBuilder()
+        .setTitle('Arrest Command was ran.')
+        .addFields(
+            { name: 'User', value: `${interaction.user.username}` },
+            { name: 'UserID', value: `${interaction.user.id}` }
+        )
+        .setTimestamp()
+
+        const Loggingchannel = interaction.client.channels.cache.get('1200946142498799667'); 
+        Loggingchannel.send({ embeds: [LogsEmbed] });
+
+        const ArrestEmbed = new EmbedBuilder()
+            .setTitle('Arrested')
+            .setColor('Blurple')
+            .addFields(
+                { name: "Officer", value: `<@${interaction.user.id}> (${interaction.user.id})` },
+                { name: "Suspect's Username", value: `${username}` },
+                { name: "Duration", value: `${duration}` },
+                { name: "Evidence", value: `${evidence}` },
+                { name: "Reason", value: `${reason}` }
+            )
+            .setTimestamp()
+
+        const ArrestLogChannel1 = interaction.client.channels.cache.get('1205608242353541180')
+        const ArrestLogChannel2 = interaction.client.channels.cache.get('1208798374950604912')
+        ArrestLogChannel1.send({ embeds: [ArrestEmbed] })
+        ArrestLogChannel2.send({ embeds: [ArrestEmbed] })
+        return interaction.editReply({ content: 'Your arrest was logged!', ephemeral: true })
+    }
+}
---
+++ commands/User/bot.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js')
+const fs = require('fs')
+
+function formatUptime(uptime) {
+    const days = Math.floor(uptime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((uptime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((uptime % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((uptime % (1000 * 60)) / 1000);
+    
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+function Version(version) { 
+    fs.readFile('package.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading package.json:', err);
+            return;
+        }
+        const packageJson = JSON.parse(data);
+        version = packageJson.version;
+    });
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('bot-info')
+    .setDescription('All the info you need to know about the bot.'),
+    async execute(interaction) {
+        await interaction.deferReply()
+
+        const uptime = interaction.client.uptime
+        let version
+
+        const InformationEmbed = new EmbedBuilder()
+        .setTitle('Information | The Realm Watcher')
+        .setColor('Blurple')
+        .addFields(
+            { name: 'Latency:', value: `${Date.now() - interaction.createdTimestamp}ms`, inline: true },
+            { name: 'API Latency:', value: `${interaction.client.ws.ping}ms`, inline: true },
+            { name: 'Uptime:', value: `${formatUptime(uptime)}`, inline: true },
+            { name: 'Version:', value: `${Version(version)}`, inline: true },
+            { name: 'MongoDB:', value: 'Operational', inline: true },
+            { name: 'Creator:', value: 'SirNotEthan', inline: true }
+        )
+        .setTimestamp()
+
+        return interaction.editReply({ embeds: [InformationEmbed]});
+    }
+}
