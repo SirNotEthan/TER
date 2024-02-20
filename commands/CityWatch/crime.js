9cba209edb0324c30bd1da6aff78f13c5300996b
SirNotEthan <ethn.bannister15@gmail.com>
2024-02-20 11:31:13 +0000

    New Crime Report Update!

---
+++ commands/CityWatch/crime.js
@@ -0,0 +1,64 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('crime-report')
+    .setDescription('Report a crime that has occured in-game'),
+    async execute(interaction) {
+        await interaction.deferReply()
+
+        try {
+            const CreateReport = new ButtonBuilder()
+            .setLabel('Create Report')
+            .setCustomId('CreateCrimeReportButton')
+            .setStyle(ButtonStyle.Danger)
+
+            const ViewReport = new ButtonBuilder()
+            .setLabel('View Reports')
+            .setCustomId('ViewCrimeReportButton')
+            .setStyle(ButtonStyle.Secondary)
+
+            const StartRow = new ActionRowBuilder()
+            .addComponents(CreateReport, ViewReport)
+
+            await interaction.editReply({ content: " ", components: [StartRow], ephemeral: true })
+        } catch (err) {
+            console.log('Error Occurred: ', err)
+        }
+
+        const mainFilter = (interaction) => interaction.customId === 'CreateCrimeReportButton' || interaction.customId === 'ViewCrimeReportButton'
+
+        const mainCollector = interaction.channel.createMessageComponentCollector({ filter: mainFilter });
+
+        mainCollector.on('collect', async (interaction) => {
+            if (interaction.customId === 'CreateCrimeReportButton') {
+                const CreateReportModal = new ModalBuilder()
+			    .setCustomId('CreateCrimeReportModal')
+			    .setTitle('Create Crime Report');
+
+                const reasonInput = new TextInputBuilder()
+                .setCustomId('reasonInput')
+                .setLabel('Reason for Crime Report')
+                .setStyle(TextInputStyle.Paragraph)
+                
+                const usernameInput = new TextInputBuilder()
+                .setCustomId('usernameInput')
+                .setLabel('Roblox Username of suspect')
+                .setStyle(TextInputStyle.Short)
+
+                const evidenceInput = new TextInputBuilder()
+                .setCustomId('evidenceInput')
+                .setLabel('Evidence of Crime')
+                .setStyle(TextInputStyle.Paragraph)
+
+                const firstActionRow = new ActionRowBuilder().addComponents(reasonInput)
+                const secondActionRow = new ActionRowBuilder().addComponents(usernameInput)
+                const thirdActionRow = new ActionRowBuilder().addComponents(evidenceInput)
+
+                CreateReportModal.addComponents(firstActionRow, secondActionRow, thirdActionRow)
+
+                await interaction.showModal(CreateReportModal)
+            }
+        });
+    }
+}
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,30 +1,87 @@
-const { Events, Collection } = require('discord.js');
+const { Events, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 const Profile = require('../models/economySchema');
 const HouseProfile = require('../models/houseEconomySchema');
 
 module.exports = {
-	name: Events.InteractionCreate,
-	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        if (interaction.isCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
 
-		const command = interaction.client.commands.get(interaction.commandName);
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
+            try {
+                await command.execute(interaction, interaction.client); 
+            } catch (error) {
+                console.error(error);
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+                } else {
+                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                }
+            }   
+        } else if (interaction.isButton()) {
+            console.log('Hello World')
+        } else if (interaction.isModalSubmit()) {
+            if (interaction.customId === 'CreateCrimeReportModal') {
+                const reasonValue = interaction.fields.getTextInputValue('reasonInput');
+                const usernameValue = interaction.fields.getTextInputValue('usernameInput');
+                const evidenceValue = interaction.fields.getTextInputValue('evidenceInput');
 
-		try {
-			await command.execute(interaction, interaction.client); 
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-			}
-		}	
 
-		if (!interaction.isButton()) return;
-	},
+                const ConfirmationEmbed = new EmbedBuilder()
+                    .setTitle('Crime Report Confirmation')
+                    .setColor('Orange')
+                    .setDescription('The following information will be submitted.')
+                    .addFields(
+                        { name: 'Author:', value: `<@${interaction.user.id}> (${interaction.user.id})` },
+                        { name: 'Suspect:', value: `${usernameValue}` },
+                        { name: 'Evidence:', value: `${evidenceValue}`},
+                        { name: 'Reason:', value: `${reasonValue}`}
+                    )
+                    .setTimestamp();
+
+                const CrimeConfirmationButton = new ButtonBuilder()
+                    .setLabel('Confirm')
+                    .setCustomId('CrimeConfirmationButton')
+                    .setStyle(ButtonStyle.Success);
+
+                const CrimeCancelButton = new ButtonBuilder()
+                    .setLabel('Cancel')
+                    .setCustomId('CrimeCancelButton')
+                    .setStyle(ButtonStyle.Danger);
+
+                const ConfirmationRow = new ActionRowBuilder()
+                    .addComponents(CrimeConfirmationButton, CrimeCancelButton);
+
+                await interaction.reply({ embeds: [ConfirmationEmbed], components: [ConfirmationRow] });
+            }
+
+            if (interaction.isButton()) {
+                if (interaction.customId === "CrimeConfirmationButton") {
+                    const CrimeSuccessEmbed = new EmbedBuilder()
+                        .setTitle('Crime Reported!')
+                        .setColor('Green')
+                        .setDescription('Your Crime was successfully reported')
+                        .setTimestamp();
+    
+                    await interaction.message.edit({ embeds: [CrimeSuccessEmbed], components: [] });
+    
+                    const CrimeReportChannel = interaction.client.channels.cache.get('1208214310451675210'); 
+                    await CrimeReportChannel.send({ embeds: [ConfirmationEmbed] });
+                } else if (interaction.customId === "CrimeCancelButton") {
+                    const CrimeCancelledEmbed = new EmbedBuilder()
+                        .setTitle('Crime Report Cancelled')
+                        .setColor('Red')
+                        .setDescription('Your Crime Report was not submitted.')
+                        .setTimestamp();
+    
+                    await interaction.message.edit({ embeds: [CrimeCancelledEmbed], components: [] });
+                }
+            }
+        }
+    }
 };
